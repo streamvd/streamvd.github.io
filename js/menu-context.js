2ba5040119011e25defd2d3850356581c7628744
streamvd <streamlinkvd@gmail.com>
2026-09-03 17:48:33 -0400

    Delete js/menu-context.js

--- js/menu-context.js
+++
@@ -1,5 +0,0 @@
-
-        // Prevenir o menu de contexto ao segurar o dedo na tela
-        document.addEventListener('contextmenu', function(event) {
-            event.preventDefault(); // Impede o menu de contexto
-        });
